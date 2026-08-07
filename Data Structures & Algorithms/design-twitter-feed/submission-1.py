9200417f633eeb8402287d523d6225990832dad5
raja <69231336+Rajasimhareddybolla@users.noreply.github.com>
2026-08-07 19:53:50 +0530

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.py
@@ -0,0 +1,27 @@
+class Twitter:
+
+    def __init__(self):
+        self.posts = [] # cronological reverse order 
+        self.connections = {} # it will be follower : set(follower id ) 
+
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.posts.append((userId , tweetId))
+        
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+
+        for infId , postId in self.posts[::-1]:
+            if infId in self.connections.get(userId , ()) or infId == userId:
+                res.append(postId)
+                if len(res) == 10:
+                    break
+        return res 
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.connections:
+            self.connections[followerId] = set()
+        self.connections[followerId].add(followeeId)
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.connections :
+            self.connections[followerId].discard(followeeId)
